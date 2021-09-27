332dd46d3d4b0f180f48471c9db65da45ed99c0d
Avishrant Sharma <avishrants@gmail.com>
2021-09-27 10:22:50 +0530

    Update CORS settings

--- backend/settings.py
+++ backend/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -138,5 +140,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 django_heroku.settings(locals())
