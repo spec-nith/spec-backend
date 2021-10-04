ff5936761e7a0baf6ae16c1ac8b77bb0befc6bdd
Avishrant Sharma <avishrants@gmail.com>
2021-10-04 17:38:20 +0530

    Update project settings for Heroku
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEVELOPMENT", True)
+DEBUG = True if os.getenv("DEVELOPMENT", "True") == "True" else False
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
