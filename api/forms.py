1a970477cc80af05ef5dd70b314cff67086c5315
Avishrant Sharma <avishrants@gmail.com>
2021-10-05 23:58:50 +0530

    Update forms.py

--- api/forms.py
+++ api/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class GalleryForm(forms.Form):
     event = forms.CharField(max_length=100, label="Event")
     sub_event = forms.CharField(max_length=100, label="Sub Event", required=False)
-    date = forms.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
+    year = forms.IntegerField(label = "Year")
     zip_import = forms.FileField(label="Zip File")
 
     def __str__(self):
