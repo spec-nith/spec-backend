a3a81ae85b7e49b14ca15c41d0a1a55b35ec67a0
Avishrant Sharma <avishrants@gmail.com>
2021-10-06 13:52:53 +0530

    Update image Validation in GalleryFormView
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- api/forms.py
+++ api/forms.py
@@ -1,15 +1,18 @@
 from django import forms
 
+from api.models import Gallery
 
-class GalleryForm(forms.Form):
-    event = forms.CharField(max_length=100, label="Event")
+
+class GalleryForm(forms.ModelForm):
     sub_event = forms.CharField(max_length=100, label="Sub Event", required=False)
-    year = forms.IntegerField(label = "Year")
-    zip_import = forms.FileField(label="Zip File")
+    year = forms.IntegerField(label="Year", required=True)
+    image = forms.ImageField(
+        label="Image Files", widget=forms.ClearableFileInput(attrs={"multiple": True})
+    )
+
+    class Meta:
+        model = Gallery
+        fields = ("event", "sub_event", "year", "image")
 
     def __str__(self):
         return self.event
-
-
-class Upload(forms.Form):
-    uploaded_image = forms.ImageField()
